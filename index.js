7eb6796341653471e5ced01a1c7c4e03869303b2
Zhang Kaiyu <yeannylam@gmail.com>
2018-05-24 15:57:44 +0800

    works with little bug

---
+++ index.js
@@ -0,0 +1,29 @@
+const seeding = require('./lib/seeding.js');
+const dropping = require('./lib/dropping.js');
+
+const overallArgv = require('yargs')
+  .command('seed', 'Seed mongoDB with mongoose schema and seed data.',
+    (yargs) => {
+      yargs
+        .option('models', {
+          describe: 'Directory of mongoose model definitions',
+          demand: true
+        })
+        .option('data', {
+          describe: 'Directory of seed data',
+          demand: true
+        })
+        .option('mongourl', {
+          describe: 'URL of mongoDB instance',
+          demand: true
+        });
+    }, seeding)
+  .command('drop', 'Drop mongoDB database.', (yargs) => {
+    yargs
+      .option('mongourl', {
+        describe: 'URL of mongoDB instance'
+      });
+  }, dropping)
+  .argv;
+
+module.exports = () => overallArgv;
