d0affce5339a2d11d4179f20cecb0ea9a5a7c93e
Zhang Kaiyu <yeannylam@gmail.com>
2019-04-26 14:18:16 +0800

    use args as list

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 #!/usr/bin/env node
+const path = require('path');
 const findDominantFile = require('find-dominant-file');
 const loadConfig = require('./lib/loadConfig');
 const showHelp = require('./lib/showHelp');
 const showVersion = require('./lib/showVersion');
+const getModelFileList = require('./lib/getModelFileList');
+const seed = require('./lib/seed');
+const unseed = require('./lib/unseed');
+const reporter = require('./lib/report');
 
 const startup = async function() {
 
   // Locate project root directory
   const projRoot = findDominantFile(process.cwd(), 'package.json', true);
 
-  // Get config object
+  // Parsing and getting settings
   const [command, args, options] = loadConfig(projRoot);
 
   // Show help and exit
@@ -24,6 +29,49 @@ const startup = async function() {
     return;
   }
 
+  // Check command availability
+  if (!['seed', 'unseed'].includes(command)) {
+    throw new Error(`Unknown command '${command}'.`);
+  }
+
+  // Requires data directory
+  if (!options.data) {
+    throw new Error('Data directory is required.');
+  }
+  options.data = path.resolve(projRoot, options.data);
+
+  // Get model file list
+  const modelFileList =
+    getModelFileList(projRoot, options.models, options.modelBaseDirectory);
+
+  // Connect mongoose
+  const mongoose = require('mongoose');
+  const connection = await mongoose.connect(options.db, {
+    useNewUrlParser: true
+  });
+
+  try {
+    // Load model files
+    modelFileList.forEach(require);
+
+    // Execute command
+    if (command === 'seed') await seed({
+      mongoose,
+      report: reporter,
+      args,
+      options
+    });
+    if (command === 'unseed') await unseed({
+      mongoose,
+      report: reporter,
+      args,
+      options
+    });
+  } catch(e) {
+    console.log(e);
+  } finally { // Close connection and exit
+    await connection.close();
+  }
 };
 
 if (require.main === module) {
