cba3318c14dfa1f0eb521bcf5c2136dafa0e41c9
Zhang Kaiyu <yeannylam@gmail.com>
2019-04-26 12:41:05 +0800

    rename into seedgoose

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const drop = require('./lib/drop');
 const withProjectOptions = require('./lib/withProjectOptions');
 
 const argv = require('yargs')
-  .pkgConf('nonula')
+  .pkgConf('seedgoose')
   .config(withProjectOptions({}))
   .command('seed', 'Seed mongoDB with mongoose schema and seed data.',
     (yargs) => {
