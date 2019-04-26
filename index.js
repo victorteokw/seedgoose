5690186210221af257399f84c37782fe5e160476
Zhang Kaiyu <yeannylam@gmail.com>
2019-04-26 13:12:43 +0800

    remove old files

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
-const seed = require('./lib/seed');
-const drop = require('./lib/drop');
-const withProjectOptions = require('./lib/withProjectOptions');
+#!/usr/bin/env node
+const findDominantFile = require('find-dominant-file');
+const loadConfig = require('./lib/loadConfig');
+const showHelp = require('./lib/showHelp');
+const showVersion = require('./lib/showVersion');
 
-const argv = require('yargs')
-  .pkgConf('seedgoose')
-  .config(withProjectOptions({}))
-  .command('seed', 'Seed mongoDB with mongoose schema and seed data.',
-    (yargs) => {
-      yargs
-        .option('models', {
-          describe: 'Directory of mongoose model definitions',
-          demand: true
-        })
-        .option('data', {
-          describe: 'Directory of seed data',
-          demand: true
-        })
-        .option('mongourl', {
-          describe: 'URL of mongoDB instance',
-          demand: true
-        });
-    }, seed)
-  .command('drop', 'Drop mongoDB database.', (yargs) => {
-    yargs
-      .option('mongourl', {
-        describe: 'URL of mongoDB instance'
-      });
-  }, drop)
-  .help('help')
-  .alias('s', 'seed')
-  .alias('d', 'drop')
-  .argv;
+const startup = async function() {
 
-module.exports = () => argv;
+  // Locate project root directory
+  const projRoot = findDominantFile(process.cwd(), 'package.json', true);
+
+  // Get config object
+  const [command, args, options] = loadConfig(projRoot);
+
+  // Show help and exit
+  if (options.help) {
+    showHelp();
+    return;
+  }
+
+  // Show version and exit
+  if (options.version) {
+    showVersion();
+    return;
+  }
+
+};
+
+if (require.main === module) {
+  startup();
+}
+
+module.exports = startup();
