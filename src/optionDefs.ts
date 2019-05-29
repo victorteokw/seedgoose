ec019768a9cb31235811cb59fc6328303a35bf90
hao.zhang <735296233@qq.com>
2019-05-29 21:59:27 +0800

    update optionDefs

--- src/optionDefs.ts
+++ src/optionDefs.ts
@@ -1,79 +1,68 @@
-// module.exports = [
-//   {
-//     name: 'help',
-//     alias: 'h',
-//     description: 'view seedgoose\'s help',
-//     type: Boolean,
-//     defaultValue: false
-//   },
-//   {
-//     name: 'version',
-//     alias: 'v',
-//     description: 'view seedgoose\'s version',
-//     type: Boolean,
-//     defaultValue: false
-//   },
-//   {
-//     name: 'config-file',
-//     alias: 'c',
-//     description: 'the config file to load',
-//     type: String,
-//     defaultValue: '.seedgooserc'
-//   },
-//   {
-//     name: 'db',
-//     alias: 'd',
-//     description: 'the database connection url',
-//     type: String,
-//     defaultValue: undefined
-//   },
-//   {
-//     name: 'models',
-//     alias: 'm',
-//     description: 'model files matcher',
-//     type: String,
-//     defaultValue: undefined
-//   },
-//   {
-//     name: 'model-base-directory',
-//     alias: 'b',
-//     description: 'where to execute model files matcher',
-//     type: String,
-//     defaultValue: undefined
-//   },
-//   {
-//     name: 'data',
-//     alias: 's',
-//     description: 'data directory',
-//     type: String,
-//     defaultValue: undefined
-//   },
-//   {
-//     name: 'mapping-table',
-//     alias: 'T',
-//     description: 'where to save readable id and actually id mapping',
-//     type: String,
-//     defaultValue: 'seedgoosemap'
-//   },
-//   {
-//     name: 'no-color-output',
-//     alias: 'C',
-//     description: 'whether output with color',
-//     type: Boolean,
-//     defaultValue: false
-//   },
-//   {
-//     name: 'verbose',
-//     alias: 'V',
-//     description: 'verbose output',
-//     type: Boolean,
-//     defaultValue: false
-//   },
-//   {
-//     name: 'silent',
-//     alias: 'S',
-//     description: 'suppress output',
-//     type: Boolean,
-//     defaultValue: false
-//   }
-// ];
+import { OptionRules } from 'type-args';
+
+const optionDefs:OptionRules = {
+  'help': {
+    alias: 'h',
+    desc: 'view seedgoose\'s help',
+    type: 'boolean',
+    default: false
+  },
+  'version': {
+    alias: 'v',
+    desc: 'view seedgoose\'s version',
+    type: 'boolean',
+    default: false
+  },
+  'configFile': {
+    alias: 'c',
+    desc: 'the config file to load',
+    type: 'string',
+    default: '.seedgooserc'
+  },
+  'db': {
+    alias: 'd',
+    desc: 'the database connection url',
+    type: 'string'
+  },
+  'models': {
+    alias: 'm',
+    desc: 'model files matcher',
+    type: 'string'
+  },
+  'modelBaseDirectory': {
+    alias: 'b',
+    desc: 'where to execute model files matcher',
+    type: 'string'
+  },
+  'data': {
+    alias: 's',
+    desc: 'data directory',
+    type: 'string'
+  },
+  'mappingTable': {
+    alias: 'T',
+    desc: 'where to save readable id and actually id mapping',
+    type: 'string',
+    default: 'seedgoosemap'
+  },
+  'noColorOutput': {
+    alias: 'C',
+    desc: 'whether output with color',
+    type: 'boolean',
+    default: false
+  },
+ 'verbose': {
+    alias: 'V',
+    desc: 'verbose output',
+    type: 'boolean',
+    default: false
+  },
+  'silent': {
+    alias: 'S',
+    desc: 'suppress output',
+    type: 'boolean',
+    default: false
+  }
+};
+
+export default optionDefs;
