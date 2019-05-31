bbe80d5cb3aac437981d7dee865e4bc48c35579f
hao.zhang <735296233@qq.com>
2019-05-31 18:21:36 +0800

    update loadConfig

--- src/loadConfig.ts
+++ src/loadConfig.ts
@@ -1,5 +1,37 @@
-function loadConfig(projRoot: string, argv: string[]): [string, string[], { [key: string]: any }] {
-  return ['seed', [], {}];
+import loadFile from 'load-any-file';
+import * as path from 'path';
+import { parse } from 'type-args';
+import optionDefs from './optionDefs';
+
+function loadConfig(
+  projRoot: string,
+  argv: string[] = process.argv
+): [string, string[], { [key: string]: any }] {
+  const [options, [command, ...args]] = parse(argv, optionDefs);
+
+  // Load config from package.json
+  const pkgJson = path.join(projRoot, 'package.json');
+  const configFromPkgJson = require(pkgJson).seedgoose;
+
+  // Load config from config file
+  let configFromConfFile;
+  try {
+    configFromConfFile = loadFile(
+      path.join(projRoot, options.configFile as string)
+    );
+  } catch (e) {
+    // ignore this
+  }
+
+  return [
+    command,
+    args,
+    {
+      ...configFromPkgJson,
+      ...configFromConfFile,
+      ...options
+    }
+  ];
 }
 
 export default loadConfig;
